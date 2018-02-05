6e39fc16aea51bc60a8a565817b941fb3a05d070
Brice Tessier Huort <tessierhuort@gmail.com>
2018-02-05 15:20:04 +0100

    install typescript

---
+++ server/router.ts
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+    res.json({message: 'ok'})
+})
+
+
+export = route
